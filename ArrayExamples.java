cd25f9e51a8372dd71f556e6cc4a367939471369
helenaonsum <130091045+helenaonsum@users.noreply.github.com>
2023-04-20 11:09:35 -0700

    Something

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -2,21 +2,38 @@
 
 public class ArrayExamples {
 
-  // Changes the input array to be in reversed order
+//OLD ERROR CODE:
+  // // Changes the input array to be in reversed order
+  // static void reverseInPlace(int[] arr) {
+  //   for(int i = 0; i < arr.length; i += 1) {
+  //     arr[i] = arr[arr.length - i - 1];
+  //   }
+  // }
+//NEW EDITED CODE:
   static void reverseInPlace(int[] arr) {
+    int current = arr[i];
     for(int i = 0; i < arr.length; i += 1) {
       arr[i] = arr[arr.length - i - 1];
     }
   }
 
-  // Returns a *new* array with all the elements of the input array in reversed
-  // order
+//OLD ERROR CODE:
+  // // Returns a *new* array with all the elements of the input array in reversed
+  // // order
+  // static int[] reversed(int[] arr) {
+  //   int[] newArray = new int[arr.length];
+  //   for(int i = 0; i < arr.length; i += 1) {
+  //     arr[i] = newArray[arr.length - i - 1];
+  //   }
+  //   return arr;
+  // }
+//NEW EDITED CODE:
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,32 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedinPlace2() {
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] {3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    assertArrayEquals(new int[] {5, 4, 3, 2, 1}, ArrayExamples.reversed(input1));
+  }
 }
+
+  // // Returns a *new* array with all the elements of the input array in reversed
+  // // order
+  // static int[] reversed(int[] arr) {
+  //   int[] newArray = new int[arr.length];
+  //   for(int i = 0; i < arr.length; i += 1) {
+  //     arr[i] = newArray[arr.length - i - 1];
+  //   }
+  //   return arr;
+  // }
